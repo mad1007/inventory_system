1cfd67eb21fadf0c098fd0a52c239ce809512725
mad1007 <moadakharraz@gmail.com>
2022-12-29 17:05:42 +0100

    ad

--- frontend/pages/articles.js
+++ frontend/pages/articles.js
@@ -1,16 +1,48 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
+import { Container, Spinner } from 'react-bootstrap'
 import ArticlesList from '../components/Articles/ArticlesList'
+import { useUser } from '../contexts/UserContext'
+import styles from '../styles/Articles.module.css'
+const Articles = () => {
 
-const artciles = () => {
+
+  const {checkAuth, user, loading, logout} = useUser()
+  const router = useRouter()
+  useEffect(() => { 
+    const authenticate = async ()=>{
+      if(!user){
+        const auth = await checkAuth()
+        if(auth){
+          console.log('Authenticated')
+        }else{
+          router.push('/login')
+        }
+      }
+    }
+    authenticate()
+
+  }, [])
+
+  if(loading || loading === undefined) return (
+    <div className='w-100 m-auto text-center mt-5'>
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+  </div>
+  )
+
   return (
     <>
       <Container className='mt-4'>
-        <h1>Liste Des Articles</h1>
-        <ArticlesList />
+        <div className={styles.grid}>
+          <h1 className='text-center' >Liste Des Articles</h1>
+          <button className='btn btn-info' >Nouveau Article</button>
+        </div>
+        {/* <ArticlesList /> */}
       </Container>
     </>
   )
 }
 
-export default artciles
+export default Articles
